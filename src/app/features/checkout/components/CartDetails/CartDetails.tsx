3f31fe8344be863bda12da5fe6ac196f5b10a4b0
zoubi <ahmadalzoubi13579@gmail.com>
2024-11-16 10:24:15 +0300

    fix: add link to checkout page

--- src/app/features/checkout/components/CartDetails/CartDetails.tsx
+++ src/app/features/checkout/components/CartDetails/CartDetails.tsx
@@ -3,22 +3,31 @@
 import { EmptyState } from '~/src/app/shared/components/EmptyState';
 import { useCartContext } from '../../context/CartContext';
 import { CartProduct } from './CartProduct';
+import { Button } from '~/src/components/ui/button';
+import { useRouter } from 'next/navigation';
+import { PATHS } from '~/src/app/shared/constants/paths';
 
 const CartDetails = () => {
+  const router = useRouter();
+
   const { products } = useCartContext();
 
   if (products.length === 0) return <EmptyState title='No Products in cart!' />;
 
   return (
-    <section className='text-left space-y-5 my-5'>
+    <section className='text-left my-5'>
       <h1 className='text-center text-4xl'>Shopping Cart</h1>
 
-      <div className='flex flex-wrap my-3'>
+      <div className='flex flex-wrap mt-10'>
         {products.map(product => (
           <div key={product.id} className='basis-full sm:basis-6/12 lg:basis-4/12 xl:basis-3/12 p-1'>
             <CartProduct cartProduct={product} />
           </div>
         ))}
+      </div>
+
+      <div className='flex justify-center mt-20'>
+        <Button onClick={() => router.push(`${PATHS.CHECKOUT}`)}>Go To Checkout</Button>
       </div>
     </section>
   );
